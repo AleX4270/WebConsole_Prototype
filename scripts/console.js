bb1398ff11c4517005a2bc703c19ad79b6018a9d
AleX4270 <aleksr427@gmail.com>
2022-08-20 18:27:10 +0200

    initial commit with working input

---
+++ scripts/console.js
@@ -0,0 +1,51 @@
+class Console
+{
+    constructor()
+    {
+        this.cform = document.querySelector(".consoleWindow");
+
+        this.initInputField();
+        this.initConsoleInteraction();
+    }
+
+    initInputField()
+    {
+        this.inputBox = document.createElement("div");
+        this.inputBox.classList.add("inputBox")
+
+        this.cform.append(this.inputBox);
+
+        this.cin = document.createElement("input");
+        this.cin.classList.add("consoleInput");
+        this.cin.maxLength = 100;
+        this.cin.type = "text";
+        this.cin.autofocus = true;
+
+        this.inputBox.append(this.cin);
+
+        const sign = document.createElement("span");
+        sign.classList.add("inputMark");
+        sign.innerHTML = "&gt;";
+        this.cin.before(sign);
+
+    }
+
+    initConsoleInteraction()
+    {
+        this.cform.addEventListener("submit", (event) => {
+            event.preventDefault();
+            this.handleConsoleInput();
+        });
+    }
+
+    handleConsoleInput()
+    {
+        let command = this.cin.value;
+        this.cin.value = "";
+        //Send the string to the parser
+        //Get the result of executing the command
+        this.inputBox.insertAdjacentHTML('beforebegin', command + '</br>');
+    }
+}
+
+const cmd = new Console();
